c859cab6f3a44949f86246686167c532b445f474
Sanchit Bhatnagar <sanbht23@gmail.com>
2021-07-04 02:47:22 +0530

    Updates

---
+++ precache-manifest.02ac8c66284396b747daf57560110cee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fa524d72ddaf7008828",
+    "url": "/Seasons/static/css/main.efef6092.chunk.css"
+  },
+  {
+    "revision": "0fa524d72ddaf7008828",
+    "url": "/Seasons/static/js/main.0fa524d7.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/Seasons/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "d437c792baba5f999744",
+    "url": "/Seasons/static/js/runtime~main.d437c792.js"
+  },
+  {
+    "revision": "789d513857749b161ac9b54e9513e698",
+    "url": "/Seasons/index.html"
+  }
+];
